38bfa6293a73c45b3a917bfe0944dfe28241cfbf
ydarissep <60859033+ydarissep@users.noreply.github.com>
2023-09-22 08:51:01 +0200

    Update global.js

--- src/global.js
+++ src/global.js
@@ -596,7 +596,7 @@ async function useBackup(){
     if(!localStorage.getItem("locations")){
         await localStorage.setItem("locations", LZString.compressToUTF16(JSON.stringify(locations)))
     }
-    window.locationsTracker = []
+    locationsTracker = []
     Object.keys(locations).forEach(zone => {
         Object.keys(locations[zone]).forEach(method => {
             Object.keys(locations[zone][method]).forEach(speciesName => {
