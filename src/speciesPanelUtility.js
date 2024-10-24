898dd686297450acd4fb6b27e6b5dbb634a9fd1b
ydarissep <60859033+ydarissep@users.noreply.github.com>
2024-10-24 12:37:07 +0200

    check if evo info is an empty string

--- src/speciesPanelUtility.js
+++ src/speciesPanelUtility.js
@@ -473,7 +473,7 @@ function createClickableImgAndName(speciesName, evoConditions = false, showName 
             evoCondition.innerText = `Level Up (${sanitizeString(evoConditions[1]).replace(/Mapsec */i, "")})`
         }
         else{
-            evoCondition.innerText = `${sanitizeString(evoConditions[0])} (${sanitizeString(evoConditions[1])})`
+            evoCondition.innerText = `${sanitizeString(evoConditions[0])} (${evoConditions[1] ? sanitizeString(evoConditions[1]) : ""})`
         }
         evoCondition.className = "evoMethod"
         container.append(evoCondition)
